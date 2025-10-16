6015acd0d1456410a95c6970f90f57685572b8cf
Garena Free Fire <community.ff.garena.com@gmail.com>
2025-10-16 02:46:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5662684056-17ed6",
+  "appId": "1:625696935365:web:d352870977a108e0e4cfac",
+  "apiKey": "AIzaSyAvRMZoQYrhHVEe-vrlZ2IV9N1D_g-xNLI",
+  "authDomain": "studio-5662684056-17ed6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "625696935365"
+};
